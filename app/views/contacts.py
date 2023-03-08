3aa6c0de7a772966016e10172ffe19628c3ecc7e
Gurgen Abramyan <gurgen.abramyan.1995@gmail.com>
2023-03-09 00:38:38 +0300

    Удалил mypy.

---
+++ app/views/contacts.py
@@ -0,0 +1,16 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+from app.models import City
+
+
+class ContactsView(View):
+    """Страница контактов."""
+
+    def get(self, request: HttpRequest):
+        """Получение страницы контактов."""
+        cities = City.objects.filter(is_active=True)
+
+        context = {"cities": cities}
+        return render(request, "contacts.html", context)
