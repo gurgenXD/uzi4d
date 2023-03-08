46eb4d283c71cff5f4cb9a556e8048b30a500086
Gurgen Abramyan <gurgen.abramyan.1995@gmail.com>
2023-03-09 00:41:19 +0300

    Контакты на главной.

--- app/views/index.py
+++ app/views/index.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
 
-from app.models import Specialist
+from app.models import Specialist, City
 
 
 SPECIALISTS_COUNT = 20
@@ -14,6 +14,7 @@ class IndexView(View):
     def get(self, request: HttpRequest):
         """Получение главной страницы."""
         specialists = Specialist.objects.filter(on_main=True)[:SPECIALISTS_COUNT]
+        cities = City.objects.filter(is_active=True)
 
-        context = {"specialists": specialists, "promotions": [], "cities": []}
+        context = {"specialists": specialists, "promotions": [], "cities": cities}
         return render(request, "index.html", context)
